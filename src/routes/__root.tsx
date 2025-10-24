6297d48b77cb9e6488c81f0b65570286a35ea011
Landon Durnan <landondurnan@gmail.com>
2025-10-24 10:25:36 -0700

    refactor: Simplify RootLayout by removing navigation links and restructuring layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,19 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { BuilderProvider } from "@/context/BuilderContext";
 import { ModeToggle } from "@/components/builder/ModeToggle";
 
 const RootLayout = () => (
   <BuilderProvider>
-    <div className="p-2 flex gap-2 justify-between items-center">
-      <div className="flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/docs/forms" className="[&.active]:font-bold">
-          Docs
-        </Link>
-      </div>
-      <ModeToggle />
+    <div className="flex flex-col h-screen">
+      <header className="p-4 flex gap-2 justify-between items-center border-b">
+        <h1 className="text-lg font-semibold">Custom Form Builder</h1>
+        <ModeToggle />
+      </header>
+      <main className="flex-1 overflow-auto">
+        <Outlet />
+      </main>
     </div>
-    <hr />
-    <Outlet />
     <TanStackRouterDevtools />
   </BuilderProvider>
 );
